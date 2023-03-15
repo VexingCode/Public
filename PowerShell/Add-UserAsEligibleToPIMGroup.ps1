c79dda3622a1c6384674ece132db91dba56451e0
Benteu <Ahnamataeus.Benteu@seattle.gov>
2023-03-15 16:14:56 -0700

    Typo.

--- PowerShell/Add-UserAsEligibleToPIMGroup.ps1
+++ PowerShell/Add-UserAsEligibleToPIMGroup.ps1
@@ -14,8 +14,8 @@ Function Add-UserAsEligibleToPIMGroup {
     .PARAMETER GroupName
         Specify the name of the group you wish to grant the eligible PIM permission to.
     .PARAMETER UserPrincipalName
-        Specify the user Principal name of the user you wish to have PIM added to. Since the UPN is
-        unique, we are relying on this rather than the SAMAccountName for synced accounts, or usernames.
+        Specify the User Principal Name of the user you wish to have PIM added to. Since the UPN is
+        unique, we are relying on this rather than the SAMAccountName, or usernames, for synced accounts.
     .EXAMPLE
         PS C:\> Add-UserAsEligibleToPIMGroup -GroupName 'PIM-RBAC-Helpdesk-Tier-1' -UserPrincipalName 'ADMDoeJohn@contoso.onmicrosoft.com'
         
@@ -26,6 +26,10 @@ Function Add-UserAsEligibleToPIMGroup {
         Author:         Ahnamataeus Vex
         Version:        1.0.0
         Release Date:   2023-03-08
+        To-Do:
+            Validate that the group is PIM enabled
+            Validate that the user is not already a member
+            Perhaps refresh membership if they are a member, but < X days left
     #>
 
     [CmdletBinding()]
@@ -37,6 +41,67 @@ Function Add-UserAsEligibleToPIMGroup {
         [string]
         $UserPrincipalName
     )
+
+    # Ensure the AzureADPreview module is installed and loaded
+    Function Install-AzureADPreviewModule {
+        Write-Verbose 'Validating if the standard AzureAD module is imported...'
+        If (Get-Module -Name AzureAD) {
+            Write-Verbose 'AzureAD is imported; removing it.'
+            Disconnect-AzureAD
+            Remove-Module -Name AzureAD
+        }
+        Else {
+            Write-Verbose 'The AzureAD module is not imported. Proceeding...'
+        }
+    
+        Write-Verbose 'Grabbing the version of AzureADPreview from the gallery...'
+        $GalleryModuleVersion = (Find-Module -Name AzureADPreview).Version
+        Write-Verbose "Gallery version is: $GalleryModuleVersion"
+        Write-Verbose 'Grabbing the version of AzureADPreview that is installed (if any)...'
+        $InstalledModuleVersion = (Get-InstalledModule AzureADPreview -ErrorAction SilentlyContinue).Version
+        If ($null -eq $InstalledModuleVersion) {
+            $InstalledModuleVersion = 'Not installed.'
+        }
+        Write-Verbose "Installed version is: $InstalledModuleVersion"
+        Write-Verbose 'Comparing the two to see if the gallery is newer...'
+        If (!($InstalledModuleVersion -ge $GalleryModuleVersion)) {
+            Write-Verbose 'Gallery version is newer, or its not installed.'
+            Write-Verbose 'Installing the AzureADPreview module.'
+            Install-Module AzureADPreview -Force -AllowClobber
+            Write-Verbose 'Importing the AzureADPreview module...'
+            Import-Module AzureADPreview -Force
+            Write-Verbose 'Connecting to AzureADPreview. Please ensure you are using a privileged account.'
+            Try {
+                Connect-AzureAD -ErrorAction Stop | Out-Null
+            }
+            Catch {
+                Write-Warning 'Failed to Connect-AzureAD.'
+                break
+            }
+        }
+        Else {
+            Write-Verbose 'AzureADPreview module is current.'
+            If (!(Get-Module -Name AzureADPreview)) {
+                Import-Module -Name AzureADPreview -Force
+            }
+            Try {
+                $aadConnectionCheck = Get-AzureADTenantDetail
+                $tenant = $aadConnectionCheck.DisplayName
+                Write-Verbose "AzureAD already connected to: $tenant"
+                Write-Verbose "Skipping connection step."
+            } 
+            Catch [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException] {
+                Write-Verbose 'Connecting to AzureAD. Please ensure you are using a privileged account.'
+                Try {
+                    Connect-AzureAD -ErrorAction Stop | Out-Null
+                }
+                Catch {
+                    Write-Warning 'Failed to Connect-AzureAD.'
+                    break
+                }
+            }
+        }
+    }
 
     # Get the GroupID
     $groupID = (Get-AzureADGroup -Filter "displayName eq '$GroupName'").ObjectID
