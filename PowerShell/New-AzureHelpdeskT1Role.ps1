ffd1f2688a338ceb935c62c828fcddd32ff58601
Ahnamataeus Vex <100492095+VexingCode@users.noreply.github01.com>
2023-04-24 14:24:46 -0700

    Updated perms.

--- PowerShell/New-AzureHelpdeskT1Role.ps1
+++ PowerShell/New-AzureHelpdeskT1Role.ps1
@@ -7,10 +7,23 @@ $templateId = (New-Guid).Guid
 $allowedResourceAction =
 @(
     "microsoft.directory/bitlockerKeys/key/read", # Read bitlocker metadata and key on devices.
+    "microsoft.directory/bitlockerKeys/metadata/read", # Read bitlocker key metadata on devices.
+    "microsoft.directory/deviceManagementPolicies/standard/read", # Read standard properties on device management application policies.
+    "microsoft.directory/deviceRegistrationPolicy/standard/read", # Read standard properties on device registration policies.
     "microsoft.directory/devices/registeredOwners/update", # Update registered owners of devices.
     "microsoft.directory/devices/registeredUsers/update", # Update registered users of devices.
-    "microsoft.directory/deviceManagementPolicies/standard/read", # Read standard properties on device management application policies.
-    "microsoft.directory/deviceRegistrationPolicy/standard/read" # Read standard properties on device registration policies.
+    "microsoft.directory/devices/standard/read", # Read basic properties on devices.
+    "microsoft.directory/users/appRoleAssignments/read", # Read application role assignments for users.
+    "microsoft.directory/users/deviceForResourceAccount/read", # Read deviceForResourceAccount of users.
+    "microsoft.directory/users/directReports/read", # Read the direct reports for users.
+    "microsoft.directory/users/identities/read", # Read identities of users.
+    "microsoft.directory/users/licenseDetails/read", # Read license details of users.
+    "microsoft.directory/users/manager/read", # Read manager of users.
+    "microsoft.directory/users/memberOf/read", # Read the group memberships of users.
+    "microsoft.directory/users/ownedDevices/read", # Read owned devices of users.
+    "microsoft.directory/users/registeredDevices/read", # Read registered devices of users.
+    "microsoft.directory/users/scopedRoleMemberOf/read", # Read user's membership of an Azure AD role, that is scoped to an administrative unit.
+    "microsoft.directory/users/standard/read" # Read basic properties on users.
 )
 $rolePermissions = @{'allowedResourceActions'= $allowedResourceAction}
  
