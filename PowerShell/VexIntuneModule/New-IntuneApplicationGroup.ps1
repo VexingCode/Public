13d7dd9820175de8a7ce94b7262006be6daf385d
Ahnamataeus Vex <100492095+VexingCode@users.noreply.github.com>
2024-09-10 09:31:18 -0700

    Added help. Fixed typo in #Requires and ExclusionGroup section.

--- PowerShell/VexIntuneModule/New-IntuneApplicationGroup.ps1
+++ PowerShell/VexIntuneModule/New-IntuneApplicationGroup.ps1
@@ -1,4 +1,145 @@
 Function New-IntuneApplicationGroup {
+    <#
+    .SYNOPSIS
+        Function to standardize the creation of Application deployment groups for Intune.
+    .DESCRIPTION
+        This function facilitations the creation of standardized groups for Application
+        deployments in Intune. It can create "Required" (AIR) and "Available" (AIA) install
+        groups, as well as "Update" (AUD) and "Uninstall" (AUR) groups. Additionally, it
+        can create exclusion groups (ExGrp) for each of those.
+    .PARAMETER ProductVendor
+        Mandatory parameter to specify the product vendor.
+        
+        NOTE: Do not worry about using special characters, as the script will strip them out.
+        Spaces will be replaced with hyphens.
+    .PARAMETER ProductName
+        Mandatory parameter to specify the product name.
+
+        NOTE: Do not worry about using special characters, as the script will strip them out.
+        Spaces will be replaced with hyphens.
+    .PARAMETER DeploymentType
+        Mandatory parameter to specify which type of deployment group you would like to create. 
+        It also has the option of creating all deployment type groups, if needed (unlikely).
+
+        This parameter is a validated set, and the options are:
+            Available
+            Required
+            Update
+            Uninstall
+            All
+    .PARAMETER DeploymentTarget
+        Mandatory parameter to specify your deployment target for the group. It also has the 
+        option of creating all deployment target groups, if needed.
+
+        This parameter is a validated set, and the options are:
+            Device
+            User
+            Both
+    .PARAMETER OperatingSystem
+        Mandatory parameter to specify the operating system this application is built and 
+        deployed for.
+
+        This parameter is a validated set, and the options are (Friendly Name = Value):
+            Windows = Win
+            Android = Drd
+            MacOS = MacOS
+            iOS/iPadOS = iOS
+    .PARAMETER ExclusionGroup
+        Optional parameter to specify if you want the function to create an exclusion group or
+        not.  If the parameter is not specified, no exclusion group is created. 
+        
+        This parameter is a validated set, and the options are:
+            Yes = Only an exclusion group will be created. Use this if you already created
+            a deployment group of the same type
+            Both = A deployment group, and an exclusion group will be created at the same time
+    .EXAMPLE
+        C:\> New-IntuneApplicationGroup -ProductVendor 'Contoso Co.' -ProductName 'Example Program' -DeploymentType Required -DeploymentTarget Device -OperatingSystem Win (-ExclusionGroup Both)
+
+        This will create a required deployment group, targeting devices, for the product specified.
+        The end result(s) will appear as below:
+
+        Group name: MEM-Win-AIR-Contoso-Co-Example-Program-D
+        Group description: Required install group targeting devices, for the Contoso Co. Example Program application.
+
+        If the "-ExclusionGroup Both" parameter|value is specified, the below group will also be
+        created:
+
+        Group name: MEM-Win-AIR-ExGrp-Contoso-Co-Example-Program-D
+        Group description: Use for exclusions to the required install group targeting devices, for the Contoso Co. Example Program application.
+    .EXAMPLE
+        C:\> New-IntuneApplicationGroup -ProductVendor 'Contoso Co.' -ProductName 'Example Program' -DeploymentType Available -DeploymentTarget User -OperatingSystem Win (-ExclusionGroup Both)
+
+        This will create an available deployment group, targeting users, for the product specified.
+        The end result(s) will appear as below:
+
+        Group name: MEM-Win-AIA-Contoso-Co-Example-Program-U
+        Group description: Available install group targeting users, for the Contoso Co. Example Program application.
+
+        If the "-ExclusionGroup Both" parameter|value is specified, the below group will also be
+        created:
+        
+        Group name: MEM-Win-AIA-ExGrp-Contoso-Co-Example-Program-U
+        Group description: Use for exclusions to the available install group targeting users, for the Contoso Co. Example Program application.
+    .EXAMPLE
+        C:\> New-IntuneApplicationGroup -ProductVendor 'Contoso Co.' -ProductName 'Example Program' -DeploymentType Update -DeploymentTarget Device -OperatingSystem Win (-ExclusionGroup Both)
+
+        This will create a required update deployment group, targeting devices, for the product specified.
+        The end result(s) will appear as below:
+
+        Group name: MEM-Win-AUD-Contoso-Co-Example-Program-D
+        Group description: Required update group targeting devices, for the Contoso Co. Example Program application.
+
+        If the "-ExclusionGroup Both" parameter|value is specified, the below group will also be
+        created:
+        
+        Group name: MEM-Win-AUD-ExGrp-Contoso-Co-Example-Program-D
+        Group description: Use for exclusions to the required update group targeting devices, for the Contoso Co. Example Program application.
+    .EXAMPLE
+        C:\> New-IntuneApplicationGroup -ProductVendor 'Contoso Co.' -ProductName 'Example Program' -DeploymentType Uninstall -DeploymentTarget Device -OperatingSystem Win (-ExclusionGroup Both)
+
+        This will create a required uninstall deployment group, targeting devices, for the product specified.
+        The end result(s) will appear as below:
+
+        Group name: MEM-Win-AUR-Contoso-Co-Example-Program-D
+        Group description: Required uninstall group targeting devices, for the Contoso Co. Example Program application.
+
+        If the "-ExclusionGroup Both" parameter|value is specified, the below group will also be
+        created:
+        
+        Group name: MEM-Win-AUR-ExGrp-Contoso-Co-Example-Program-D
+        Group description: Use for exclusions to the required uninstall group targeting devices, for the Contoso Co. Example Program application.
+    .EXAMPLE
+        C:\> New-IntuneApplicationGroup -ProductVendor 'Contoso Co.' -ProductName 'Example Program' -DeploymentType Required -DeploymentTarget Both -OperatingSystem Win (-ExclusionGroup Both)
+
+        This will create required install deployment groups, targeting devices and users, for the product specified.
+        The end result(s) will appear as below:
+
+        Group name: MEM-Win-AIR-Contoso-Co-Example-Program-D
+        Group description: Required install group targeting devices, for the Contoso Co. Example Program application.
+
+        Group name: MEM-Win-AIR-Contoso-Co-Example-Program-U
+        Group description: Required install group targeting users, for the Contoso Co. Example Program application.
+
+        If the "-ExclusionGroup Both" parameter|value is specified, the below group will also be
+        created:
+
+        Group name: MEM-Win-AIR-ExGrp-Contoso-Co-Example-Program-D
+        Group description: Use for exclusions to the required install group targeting devices, for the Contoso Co. Example Program application.
+
+        Group name: MEM-Win-AIR-ExGrp-Contoso-Co-Example-Program-U
+        Group description: Use for exclusions to the required install group targeting users, for the Contoso Co. Example Program application.
+    .NOTES
+        Name:           New-IntuneApplicationGroup.ps1
+        Author:         Ahnamataeus Vex
+        Version:        1.0.0
+        Release Date:   2024-09-09
+            Updated:
+                Version 1.0.1: 2024-09-10
+                    - Added the help section
+                    - Fixed typo in #Requires statement
+                    - Fixed typo in the Exclusion Group description creation.
+    #>
+    
     [CmdletBinding()]
     param (
         [Parameter(Mandatory,Position=0)]
@@ -25,10 +166,10 @@ Function New-IntuneApplicationGroup {
         $ExclusionGroup
     )
 
-    # Required -Modules Microsoft.Graph.Beta.Groups
+    #Requires -Modules Microsoft.Graph.Beta.Groups
 
     If (!(Get-MgContext)) {
-        Connect-MgGraph
+        Connect-MgGraph -NoWelcome
     }
 
     Function Clean-String {
@@ -77,7 +218,7 @@ Function New-IntuneApplicationGroup {
 
             If ($ExclusionGroup -eq 'Yes' -or $ExclusionGroup -eq 'Both') {
                 $compiledNameEx = "MEM-$OperatingSystem-$nameDT-ExGrp-$cleanVendor-$cleanName-$nameTgt"
-                $compiledDescEx = "Use for exclusions to the $($descDTLower.ToLower()) group targeting $descTgt, for the $ProductVendor $ProductName application."
+                $compiledDescEx = "Use for exclusions to the $($descDT.ToLower()) group targeting $descTgt, for the $ProductVendor $ProductName application."
 
                 $BodyEx = @{
                     DisplayName     = $compiledNameEx
