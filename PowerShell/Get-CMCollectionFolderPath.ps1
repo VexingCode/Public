673831d4a4b7c1ac8d46f214a89cec505bf0c304
Ahnamataeus Vex <Ahnamataeus.Benteu@seattle.gov>
2025-10-08 10:01:33 -0700

    Contains fixes.

--- PowerShell/Get-CMCollectionFolderPath.ps1
+++ PowerShell/Get-CMCollectionFolderPath.ps1
@@ -1,33 +1,51 @@
 Function Get-CMCollectionFolderPath {
+    [CmdletBinding(DefaultParameterSetName = 'ByName')]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'ByName')]
         [string]$CollectionName,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'ById')]
+        [string]$CollectionId,
 
         [Parameter(Mandatory = $true)]
         [string]$SiteCode
     )
 
-    # Get the collection object
-    $collection = Get-CMDeviceCollection | Where-Object { $_.Name -eq $CollectionName }
-    If (-not $collection) {
-        Write-Warning "Collection '$CollectionName' not found."
-        return
+    # Resolve collection object
+    switch ($PSCmdlet.ParameterSetName) {
+        'ByName' {
+            $collection = Get-CMCollection -Name $CollectionName
+            If (-not $collection) {
+                Write-Warning "Collection '$CollectionName' not found."
+                return
+            }
+        }
+        'ById' {
+            $collection = Get-CMCollection -Id $CollectionId
+            If (-not $collection) {
+                Write-Warning "Collection ID '$CollectionId' not found."
+                return
+            }
+        }
     }
+
+    # Determine object type: 5000 = device, 5001 = user
+    $objectType = If ($collection.CollectionType -eq 2) { 5000 } else { 5001 }
 
     # Get the container node mapping
     $containerItem = Get-WmiObject -Namespace "root\SMS\site_$SiteCode" `
         -Class SMS_ObjectContainerItem `
-        -Filter "InstanceKey = '$($collection.CollectionID)' AND ObjectType = 5000"
+        -Filter "InstanceKey = '$($collection.CollectionID)' AND ObjectType = $objectType"
 
     If (-not $containerItem) {
-        Write-Warning "No folder mapping found for collection '$CollectionName'."
+        Write-Warning "No folder mapping found for collection '$($collection.Name)'."
         return
     }
 
     # Recursively build the folder path
-    $path = @()
+    $path = New-Object System.Collections.Generic.List[string]
     $nodeID = $containerItem.ContainerNodeID
-    While ($nodeID -ne 0) {
+    while ($nodeID -ne 0) {
         $node = Get-WmiObject -Namespace "root\SMS\site_$SiteCode" `
             -Class SMS_ObjectContainerNode `
             -Filter "ContainerNodeID = $nodeID"
@@ -37,5 +55,5 @@ Function Get-CMCollectionFolderPath {
     }
 
     $folderPath = ($path -join "\")
-    Write-Output "Collection '$CollectionName' lives in folder: $folderPath"
+    Write-Output "Collection '$($collection.Name)' lives in folder: $folderPath"
 }
